594670e49dd2774c26349e58f57d061097f78d01
Pradeep Khatri <pradeepkhatri44@live.com>
2025-04-29 22:23:10 +0530

    commit Complaints CRUD
    commit Complaints CRUD

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
