944e3a92f6a4ab4cb74e16740e7c68ec1757ef8c
Pradeep Khatri <pradeepkhatri44@live.com>
2025-05-01 17:24:03 +0530

    commit for Complaints CRUD

--- backend/server.js
+++ backend/server.js
@@ -16,4 +16,8 @@ mongoose
 
 app.use("/api/auth", authRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
 app.listen(5000, () => console.log("Server running on port 5000"));
